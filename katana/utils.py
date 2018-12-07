46330c711d16676bcf1ed729377784871cbc764b
Micah Sandusky <micah.sandusky@ars.usda.gov>
2018-12-07 10:54:39 -0700

    cleaning up new utility

--- katana/utils.py
+++ katana/utils.py
@@ -7,42 +7,47 @@ import re
 
 def sample_hrrr_grib2(fp_grib, lats, lons, stns, var, dt):
     '''
-    Use wgrib2 cli to sample variables from a grib2 file at a certain lat and lon
+    Use wgrib2 cli to sample variables from a grib2 file at a certain lat and lon,
+    corresponding to weather station. Sampling is nearest neighbor.
 
     Args:
         fp_grib:    File pointer to grib2 file
-        lats:        list of lat of point
-        lons:        list of lon of point
-        var:       strings matching variables to sample
+        lats:       list of lat of point
+        lons:       list of lon of point
+        var:        strings matching variables to sample
 
     Returns:
-        vals:       list of values in same order as var
+        df:         pandas dataframe of sampled pixel values
+        df_loc:     pandas dataframe of pixel locations
     '''
+    # create list of stations out of np array from sql command
     stns = [str(stn) for stn in stns]
+    # make the columns for the dataframe
     clms = ['date_time'] + stns
 
+    # create a dataframe to store pixel values
     df = pd.DataFrame(columns=clms)
-    #print(dt)
     df['date_time'] = [dt]
-    #df.set_index('date_time', inplace=True)
-
+    # make a dataframe to store pixel coordinates
     df_loc = pd.DataFrame(columns=['latitude', 'longitude'])
 
     #vars are 2m air temp and 1 hour accm precip
     var_maps = {'air_temp': 'TMP:2 m', 'precip_intensity': 'APCP:surface'}
-    vals = {}
-    # construct command line argument
     k = var
 
+    # construct command line argument
     action = 'wgrib2  {} '.format(fp_grib)
+    # variable call
     action += ' -match "{}" '.format(var_maps[k])
     action += ' -ncpu 1 -s '
+    # add each station location
     for lon, lat in zip(lons, lats):
         action += ' -lon {} {}'.format(lon, lat)
 
-    print('\n\nTrying {}\n'.format(action))
+    print('\nTrying {}\n'.format(action))
     s = Popen(action, shell=True, stdout=PIPE, stderr=PIPE)
     idl = 0
+    # array to store values
     val = np.ones(len(lats))*np.nan
 
     real_lon = []
@@ -66,26 +71,23 @@ def sample_hrrr_grib2(fp_grib, lats, lons, stns, var, dt):
             raise ValuesError('Not enough returns')
 
         for idl, ln in enumerate(lines):
+            # get value from return string
             out = float(re.search('val=(.+?)$', ln).group(1))
+            # get coordinate of pixel from return string
             real_lon.append(float(re.search('lon=(.+?),', ln).group(1)))
             real_lat.append(float(re.search('lat=(.+?),', ln).group(1)))
 
             # convert air temp to celcius
             if k == 'air_temp':
                 out -= 273.15
-
+            # store value at staion index
             val[idl] = out
-            #idl += 1
 
-    #vals[k] = val
-
+    # loop through each station and grab value at that station
     for idl, stn in enumerate(stns):
         df[stn] = [val[idl]]
         df_loc.loc[stn, 'latitude'] = real_lat[idl]
         df_loc.loc[stn, 'longitude'] = real_lon[idl]
-
-    # vals['hrrr_lat'] = real_lat
-    # vals['hrrr_lon'] = real_lon
 
     return df, df_loc
 
