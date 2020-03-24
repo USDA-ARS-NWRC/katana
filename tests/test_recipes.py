89e9c8043b4287b0599b35a48dc0a935382a9e84
Scott Havens <scott.havens@ars.usda.gov>
2020-03-24 16:14:37 -0600

    updated the CoreConfig, recipes and create a test to ensure that the recipies are being followed

---
+++ tests/test_recipes.py
@@ -0,0 +1,94 @@
+from copy import deepcopy
+
+from inicheck.tools import cast_all_variables
+from inicheck.utilities import pcfg
+
+from tests.test_base import KatanaTestCase
+
+
+class TestRecipes(KatanaTestCase):
+    """Test the recipes 
+    """
+
+    def cast_recipes(self, config):
+        """
+        Cast the inicheck recipes
+        """
+
+        config.apply_recipes()
+        config = cast_all_variables(config, config.mcfg)
+
+        return config
+
+    def master_config(self, config):
+        """
+        Create a master config dictionary with a
+        list of the keys
+        """
+
+        master_config = {}
+        for key in config.mcfg.cfg.keys():
+            master_config[key] = list(config.mcfg.cfg[key].keys())
+
+        return master_config
+
+    def check_config(self, config, master_config):
+        """ Check that the config read by inicheck matches
+        the master_config (expected) results
+        """
+
+        # ensure that there are no
+        for key, items in master_config.items():
+            test_items = list(config.cfg[key].keys())
+
+            for item in items:
+                self.assertTrue(item in test_items)
+                test_items.remove(item)
+
+            self.assertTrue(len(test_items) == 0)
+
+    def test_hrrr_recipe(self):
+        """Test the hrrr recipe
+        """
+
+        # no changes to the base config
+        config = deepcopy(self.base_config)
+
+        # get the master config list
+        master_config = self.master_config(config)
+
+        # make changes
+        master_config['input'] = [
+                'data_type',
+                'hrrr_directory',
+                'hrrr_buffer',
+                'hrrr_num_wgrib_threads'
+            ]
+
+        self.check_config(config, master_config)
+
+
+    def test_wrf_recipe(self):
+        """Test the wrf recipe
+        """
+
+        config = self.change_config_option(
+            'input', 'data_type', 'wrf_out')
+        config = self.change_config_option(
+            'input', 'wrf_filename', './RME/input/WRF_test.nc', config)
+
+        # get the master config list
+        master_config = self.master_config(config)
+
+        # make changes
+        master_config['input'] = [
+                'data_type',
+                'wrf_filename'
+            ]
+        master_config['output'].remove('make_new_gribs')
+
+        self.check_config(config, master_config)
+
+
+
+        
