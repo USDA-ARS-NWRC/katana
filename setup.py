4185247c8dbb5a21025fc010313fe096b7585a30
Scott Havens <scott.havens@ars.usda.gov>
2020-05-04 12:16:21 -0600

    bumpversion no longer needed, using setuptools_scm and removed all the git info as it wasn't used

--- setup.py
+++ setup.py
@@ -3,49 +3,7 @@
 
 """The setup script."""
 
-import os
-import sys
-from subprocess import check_output
-
 from setuptools import setup, find_packages
-
-if sys.argv[-1] != 'test':
-    # Grab and write the gitVersion from 'git describe'.
-    gitVersion = ''
-    gitPath = ''
-
-    # get git describe if in git repository
-    print('Fetching most recent git tags')
-    if os.path.exists('./.git'):
-        try:
-            # if we are in a git repo, fetch most recent tags
-            check_output(["git fetch --tags"], shell=True)
-        except Exception:
-            print('Unable to fetch most recent tags')
-
-        try:
-            ls_proc = check_output(
-                ["git describe --tags"], shell=True, universal_newlines=True)
-            gitVersion = ls_proc
-            print('Checking most recent version')
-        except Exception:
-            print('Unable to get git tag and hash')
-    # if not in git repo
-    else:
-        print('Not in git repository')
-        gitVersion = ''
-
-    # get current working directory to define git path
-    gitPath = os.getcwd()
-
-    # git untracked file to store version and path
-    fname = os.path.abspath(os.path.expanduser('./katana/gitinfo.py'))
-
-    with open(fname, 'w') as f:
-        nchars = len(gitVersion) - 1
-        f.write("__gitPath__ = '{0}'\n".format(gitPath))
-        f.write("__gitVersion__ = '{0}'\n".format(gitVersion[:nchars]))
-        f.close()
 
 setup(
     author="USDA-ARS-NWRC",
